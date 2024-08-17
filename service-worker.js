d7d310243457cef1955603e2d9374ce352db7c0d
Muath Alsowadi <34031333+muath-ye@users.noreply.github.com>
2024-08-17 14:53:34 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'time-calculator-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './styles.css', // If you have separate CSS file
+    './script.js',  // If you have a separate JS file
+    './icons/icon-192x192.png',
+    './icons/icon-512x512.png'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
